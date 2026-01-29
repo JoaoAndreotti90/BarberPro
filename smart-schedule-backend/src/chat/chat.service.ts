d4b7572dd4a4f88e6f75dd1f988271ab252bdb06
João Andreotti <joaogilberto238@gmail.com>
2026-01-29 14:27:02 -0300

    Fix: Atualizando modelo da IA para gemini-1.5-flash

--- smart-schedule-backend/src/chat/chat.service.ts
+++ smart-schedule-backend/src/chat/chat.service.ts
@@ -16,7 +16,7 @@ export class ChatService {
     const genAI = new GoogleGenerativeAI(apiKey);
     
     this.model = genAI.getGenerativeModel({ 
-      model: 'gemini-pro', 
+      model: 'gemini-1.5-flash', 
       tools: [
         {
           functionDeclarations: [
